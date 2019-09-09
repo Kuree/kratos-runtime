7f20f54c164ce9d2cfdc3dad819b42fb6d74d326
Keyi Zhang <keyi@stanford.edu>
2019-09-08 19:54:47 -0700

    register sim working

--- src/control.cc
+++ src/control.cc
@@ -1,2 +1,43 @@
 #include "control.hh"
+#include <iostream>
 #include "std/vpi_user.h"
+
+void breakpoint_trace(uint32_t) {}
+
+PLI_INT32 initialize_server(s_cb_data*) { return 0; }
+
+PLI_INT32 teardown_server(s_cb_data*) { return 0; }
+
+void initialize_runtime() {
+    // register the initialize server when the simulation started
+    s_cb_data cb_data_init;
+    cb_data_init.reason = cbStartOfSimulation;
+    cb_data_init.cb_rtn = &initialize_server;
+    cb_data_init.obj = nullptr;
+    cb_data_init.index = 0;
+    cb_data_init.value = nullptr;
+    cb_data_init.time = nullptr;
+    vpiHandle res = vpi_register_cb(&cb_data_init);
+    if (!res) {
+        std::cerr << "ERROR: failed to register runtime initialization" << std::endl;
+    }
+    // clear the handle since we don't need it
+    vpi_free_object(res);
+
+    // register the initialize server when the simulation started
+    s_cb_data cb_data_teardown;
+    cb_data_teardown.reason = cbEndOfSimulation;
+    cb_data_teardown.cb_rtn = &teardown_server;
+    cb_data_teardown.obj = nullptr;
+    cb_data_teardown.index = 0;
+    cb_data_teardown.value = nullptr;
+    cb_data_teardown.time = nullptr;
+    res = vpi_register_cb(&cb_data_teardown);
+    if (!res) {
+        std::cerr << "ERROR: failed to register runtime initialization" << std::endl;
+    }
+    vpi_free_object(res);
+}
+
+// these are system level calls. register it to the simulator
+void (*vlog_startup_routines[])() = {initialize_runtime, nullptr};
--- src/control.hh
+++ src/control.hh
@@ -7,9 +7,6 @@ extern "C" {
 // this is the breakpoint insert by kratos for each statement
 void breakpoint_trace(uint32_t id);
 
-// this is the dpi function that will be called when the simulation starts
-void initialize_runtime();
-
 }
 
 #endif  // KRATOS_RUNTIME_CONTROL_HH
