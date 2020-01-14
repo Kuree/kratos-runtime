bf674f711ac4ce46c9bae9c79b5fd634c9a8a8ab
Keyi Zhang <keyi@stanford.edu>
2020-01-14 15:44:26 -0800

    use different container for verilator

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -9,10 +9,12 @@ docker exec -i manylinux bash -c 'cd kratos-runtime && auditwheel show dist/*'
 docker exec -i manylinux bash -c 'cd kratos-runtime && auditwheel repair dist/*'
 # remove the version dependencies
 docker exec -i manylinux bash -c 'cd /kratos-runtime/scripts && ./fix_wheel.sh'
-docker exec -i manylinux bash -c "cd /kratos-runtime/scripts && pip install dist/*.whl"
+# run in a new container where we have verilator installed
+docker run -d --name test --rm -it --mount type=bind,source="$(pwd)"/../kratos-runtime,target=/kratos-runtime keyiz/garnetflow bash
 # install the python wheel and test it using the mock
-docker exec -i manylinux bash -c "pip install pytest twine"
-docker exec -i manylinux bash -c "cd /kratos-runtime && pytest tests/"
+docker exec -i test bash -c "cd /kratos-runtime/scripts && pip install dist/*.whl"
+docker exec -i test bash -c "python -m pip install pytest twine"
+docker exec -i test bash -c "cd /kratos-runtime && python -m pytest tests/"
 
 
 echo [distutils]                                  > ~/.pypirc
