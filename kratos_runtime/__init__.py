3850405defcd88954d41ceef5559b6775595ac28
Keyi Zhang <keyi@stanford.edu>
2020-01-14 11:42:53 -0800

    refactor the python code

--- kratos_runtime/__init__.py
+++ kratos_runtime/__init__.py
@@ -1,7 +1,6 @@
 import os
-from urllib import request
-import json
 
+from .debugger import DebuggerMock
 
 def get_lib_path():
     filename = "libkratos-runtime.so"
@@ -17,93 +16,3 @@ def get_ncsim_flag():
 def get_vcs_flag():
     return "+vpi -load libkratos-runtime.so:initialize_runtime_vpi -acc+=rw"
 
-
-class DebuggerMock:
-    def __init__(self, port=8888):
-        self.port = port
-
-    def continue_(self):
-        r = request.Request("http://localhost:{0}/continue".format(self.port),
-                            method="POST")
-        try:
-            resp = request.urlopen(r)
-            return resp
-        except:
-            assert False, "Unable to continue"
-
-    def insert_breakpoint(self, filename, line_num):
-        data = json.dumps({"filename": filename, "line_num": line_num})
-        r = request.Request(
-            "http://localhost:{0}/breakpoint".format(self.port),
-            method="POST")
-        r.add_header("Content-Type'", "application/json")
-        try:
-            resp = request.urlopen(r, data)
-            return resp
-        except:
-            assert False, "Unable to insert breakpoint"
-
-    def connect(self):
-        import time
-        connected = False
-        trial = 5
-        sleep = 1
-        for _ in range(trial):
-            r = request.Request(
-                "http://localhost:{0}/status".format(self.port),
-                method="GET")
-            try:
-                request.urlopen(r)
-                connected = True
-                break
-            except:
-                time.sleep(sleep)
-                sleep *= 2
-        assert connected, "Unable to connect"
-
-    def wait_till_finish(self):
-        import time
-        while True:
-            r = request.Request(
-                "http://localhost:{0}/status".format(self.port),
-                method="GET")
-            try:
-                request.urlopen(r)
-                time.sleep(1)
-            except:
-                return
-
-    def is_paused(self):
-        r = request.Request(
-            "http://localhost:{0}/status/simulation".format(self.port),
-            method="GET")
-        try:
-            resp = request.urlopen(r)
-            status = resp.read().decode("ascii")
-            if status == "Paused":
-                return True
-        except Exception as ex:
-            return False
-
-    def wait_till_pause(self):
-        import time
-        while not self.is_paused():
-            time.sleep(1)
-
-    def get_value(self, handle_name):
-        r = request.Request(
-            "http://localhost:{0}/value/{1}".format(self.port, handle_name),
-            method="GET")
-        try:
-            resp = request.urlopen(r)
-            status = resp.read().decode("ascii")
-            return status
-        except:
-            return None
-
-
-if __name__ == "__main__":
-    mock = DebuggerMock()
-    print(mock.is_paused())
-    print(mock.get_value("mod.in"))
-    mock.continue_()
---
+++ kratos_runtime/debugger.py
@@ -0,0 +1,83 @@
+from urllib import request, error
+import json
+
+
+class DebuggerMock:
+    def __init__(self, port=8888):
+        self.port = port
+
+    def _post(self, sub_url, header=None, data=None):
+        r = request.Request("http://localhost:{0}/{1}".format(self.port, sub_url),
+                            method="POST")
+        return self.__get_data(r, header, data)
+
+    def _get(self, sub_url, header=None):
+        r = request.Request("http://localhost:{0}/{1}".format(self.port, sub_url),
+                            method="GET")
+        return self.__get_data(r, header)
+
+    @staticmethod
+    def __get_data(r, header, data=None):
+        if header is not None:
+            for k, v in header.items():
+                r.add_header(k, v)
+        try:
+            resp = request.urlopen(r, data)
+            result = resp.read()
+            if resp.code != 200:
+                return None
+            return result.decode("ascii")
+        except error.URLError as ex:
+            return None
+
+    def continue_(self):
+        r = self._post("continue")
+        assert r is not None, "Unable to continue"
+        return r
+
+    def insert_breakpoint(self, filename, line_num):
+        data = json.dumps({"filename": filename, "line_num": line_num})
+        r = self._post("breakpoint", {"Content-Type'": "application/json"}, data)
+        assert r is not None, "Unable to insert breakpoint"
+        return r
+
+    def connect(self):
+        import time
+        connected = False
+        trial = 5
+        sleep = 1
+        for _ in range(trial):
+            r = self._get("status")
+            if r is not None:
+                break
+            time.sleep(sleep)
+            sleep *= 2
+        assert connected, "Unable to connect"
+
+    def wait_till_finish(self):
+        import time
+        while True:
+            r = self._get("status")
+            if r is None:
+                break
+            time.sleep(1)
+
+    def is_paused(self):
+        r = self._get("status/simulation")
+        return r is not None and r == "Paused"
+
+    def wait_till_pause(self):
+        import time
+        while not self.is_paused():
+            time.sleep(1)
+
+    def get_value(self, handle_name):
+        r = self._get("value/" + handle_name)
+        return r
+
+
+if __name__ == "__main__":
+    mock = DebuggerMock()
+    print(mock.is_paused())
+    print(mock.get_value("mod.in"))
+    mock.continue_()
