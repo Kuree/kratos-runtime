36e47690156b93c647fff11476af57742888b1f1
Keyi Zhang <keyi@stanford.edu>
2020-01-15 10:51:07 -0800

    test our new kratos container

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -9,9 +9,12 @@ docker exec -i manylinux bash -c 'cd kratos-runtime && auditwheel show dist/*'
 docker exec -i manylinux bash -c 'cd kratos-runtime && auditwheel repair dist/*'
 # remove the version dependencies
 docker exec -i manylinux bash -c 'cd /kratos-runtime/scripts && pip install wheeltools && ./fix_wheel.sh'
-# run in a new container where we have verilator installed
-docker run -d --name test --rm -it --mount type=bind,source="$(pwd)"/../kratos-runtime,target=/kratos-runtime keyiz/garnet-flow bash
+# run the kratos container
+docker run -d --name test --rm -it --mount type=bind,source="$(pwd)"/../kratos-runtime,target=/kratos-runtime keyiz/kratos bash
+# install verilator
+docker exec -i test bash -c "apt update && apt install -y build-essential verilator"
 # install the python wheel and test it using the mock
+docker exec -i test bash -c "python3 -m pip install setuptools wheel"
 docker exec -i test bash -c "cd /kratos-runtime/scripts && python3 -m pip install dist/*.whl"
 docker exec -i test bash -c "python3 -m pip install pytest twine kratos && rm /usr/local/bin/pytest"
 docker exec -i test bash -c "cp -r /kratos-runtime/tests/ /tests/ && python3 -m pytest /tests/"
